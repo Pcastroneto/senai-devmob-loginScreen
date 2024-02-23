58c3369c78e2d9af5d65badaf3f651141356d983
Seu nome <email@dominio.com>
2024-02-22 23:07:12 -0300

    Teste

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, TextInput, Button, Image, Text } from 'react-native';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.loginContainer}>
+        <Image source={require('./download.png')} style={styles.image} resizeMode="contain"/>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Senha" secureTextEntry={true} />
+        <Button title="ENTRAR" onPress={() => {}} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0', // fundo cinza
   },
-});
+  loginContainer: {
+    backgroundColor: '#ffffff', // fundo branco
+    borderRadius: 10,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  image: {
+    width: 200,
+    height: 100,
+    marginBottom: 20,
+
+  },
+  input: {
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+};
+
+export default App;
